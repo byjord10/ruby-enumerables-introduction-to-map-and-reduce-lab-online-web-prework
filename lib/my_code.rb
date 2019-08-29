79fbf20ac098eedee30864e5ceb8ebb43b690c1b
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-29 21:15:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,8 +24,8 @@ def reduce_to_total (source_array, starting_point)
   source_array.reduce(100) { |sum, element| sum + element }
 end
 
-def reduce_to_total (source_array)
- source_array.reduce(0) { |sum, element| sum + element }
+def reduce_to_total (source_array, starting_point =0)
+ source_array.reduce(starting_point) { |sum, element| sum + element }
  end
 
 def reduce_to_total(source_array, starting_point)
