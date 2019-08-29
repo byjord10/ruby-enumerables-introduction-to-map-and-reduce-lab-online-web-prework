b559e6129cb4d61bb1c95b17cf5174c4643ff163
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-29 21:09:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,16 +25,9 @@ def reduce_to_total (source_array, starting_point)
 end
 
 def reduce_to_total (source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * source_array[i] )
-    i += 1
-end
-return new
-end 
+ source_array.reduce(0) { |sum, element| sum + element }
+ end
 
 def reduce_to_total(source_array, starting_point)
   source_array.reduce(100) { |sum, element| sum + element }
-end
 end
